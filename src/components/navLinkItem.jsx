4306ca570c256eab03a66e625f965885a3f5519e
Seyi Amusan <seyijoshua14@gmail.comgit config --global user.email seyijoshua14@gmail.com>
2024-09-16 14:59:55 +0100

    Improved step navigation buttons to track user activity

--- src/components/navLinkItem.jsx
+++ src/components/navLinkItem.jsx
@@ -1,18 +1,21 @@
+import { NavLink, useLocation } from 'react-router-dom';
 import React from 'react';
-import { Link } from 'react-router-dom';
 
+function NavLinkItem({ index, linkText, step }) {
+  const location = useLocation(); // Get the current route
 
-function NavLinkItem({index, linkText, step}) {
-    
-    return (
-        <Link to={step} className='nav-link-item-container'>
-            <button className='index'>{index}</button>
-            <div className='nav-link-text-container'>
-                <p className='step'>STEP {index}</p>
-                <p className='nav-link-text'>{linkText}</p>
-            </div>
-        </Link>
-    );
+  // Determine if this NavLink is active by checking the current URL
+  const isActive = location.pathname.includes(step);
+
+  return (
+    <NavLink to={step} className='nav-link-item-container'>
+      <button className={`index ${isActive ? 'active' : ''}`}>{index}</button>
+      <div className='nav-link-text-container'>
+        <p className='step'>STEP {index}</p>
+        <p className='nav-link-text'>{linkText}</p>
+      </div>
+    </NavLink>
+  );
 }
 
 export default NavLinkItem;
